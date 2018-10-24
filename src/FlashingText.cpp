b18df0caa2d3109f52be8b145d94716636a66cb5
Florian Lüdiger <florian.luediger@googlemail.com>
2018-10-24 21:22:04 +0200

    Finish flashing text utility

--- src/FlashingText.cpp
+++ src/FlashingText.cpp
@@ -8,13 +8,29 @@ int displayDuration = 2000;
 os_timer_t interruptTimer;
 
 void setCenteredText(String text) {
-    Serial.println("Method called");
-    Matrix::symbolInBuffer(5, letters[34 + currentText], 5);
+    int ledLength = text.length() * 6 - 1;
+    if (ledLength > MAX_IN_USE * 8)
+        return;
+
+    int *bufferContent = new int[ledLength - 1]();
+    for (unsigned int i = 0; i < text.length(); i++) {
+        for (int j = 0; j < 5; j++) {
+            bufferContent[i*6 + j] = letters[text[i] - 32][j];
+        }
+    }
+
+    int startPosition = (MAX_IN_USE * 8 - ledLength) / 2;
+
+    Matrix::symbolInBuffer(startPosition, bufferContent, ledLength);
+    delete[] bufferContent;
     Matrix::drawBuffer();
 }
 
 void updateCurrentText(void *pArg) {
+    Serial.println("updateCurrentText current text:");
     Serial.println(flashingBuffer[currentText]);
+    Serial.println("Value of currentText:");
+    Serial.println(currentText);
     setCenteredText(flashingBuffer[currentText]);
     currentText++;
     if (currentText >= wordCount)
@@ -28,19 +44,39 @@ void updateCurrentText(void *pArg) {
 * Parameter len: the number of characters
 */
 void FlashingText::setText(String text, int len) {
+    Serial.println("Input text: ");
     Serial.println(text);
-    wordCount = 0;
-    /*char *word;
-    word = strtok(text, ";");
-    do {
-        Serial.println(word);
-        flashingBuffer[wordCount] = word;
-        wordCount++;
-        word = strtok(NULL, ";");
-    } while (word != NULL);*/
 
-    Serial.println(flashingBuffer[0]);
-    Serial.println(flashingBuffer[1]);
+    if(text.length() < 1)
+        return;
+    
+    wordCount = 1;
+    for (unsigned int i = 0; i < text.length(); i++) {
+        if (text[i] == ';')
+            wordCount++;
+    }
+
+    Serial.println("Value of wordCount:");
+    Serial.println(wordCount);
+    
+    delete[] flashingBuffer;
+    flashingBuffer = new String [wordCount];
+
+    int lastSemiColon = -1;
+    int wordIndex = 0;
+    for (unsigned int i = 0; i < text.length(); i++) {
+        if (text[i] == ';') {
+            flashingBuffer[wordIndex] = text.substring(lastSemiColon+1, i);
+            lastSemiColon = i;
+            wordIndex++;
+        }
+    }
+    flashingBuffer[wordIndex] = text.substring(lastSemiColon+1);
+
+    Serial.println("Contents of flashing buffer:");
+    for (int i = 0; i < wordCount; i++)
+        Serial.println(flashingBuffer[i]);
+    Serial.println("###");
 
     currentText = 0;
 
