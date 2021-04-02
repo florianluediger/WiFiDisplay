89b442efc027bb4baedf7a3bf5f27d730ef9b776
Florian Lüdiger <florian.luediger@adesso.de>
2021-04-02 21:55:15 +0200

    Reduce interval for checking changes of the currently playing Track on Spotify

--- src/SchedulerUtils.cpp
+++ src/SchedulerUtils.cpp
@@ -6,7 +6,7 @@ Scheduler* scheduler = new Scheduler();
 Task* webserver = new Task(0, TASK_FOREVER, &WebServer::checkForRequest, scheduler);
 Task* runningtext = new Task(50, TASK_FOREVER, &RunningText::updatePosition, scheduler);
 Task* flashingtext = new Task(500, TASK_FOREVER, &FlashingText::updateCurrentText, scheduler);
-Task* spotifymonitor = new Task(10000, TASK_FOREVER, &SpotifyMonitor::checkForNewTrack, scheduler);
+Task* spotifymonitor = new Task(1000, TASK_FOREVER, &SpotifyMonitor::checkForNewTrack, scheduler);
 
 void SchedulerUtils::executeScheduler() {
     scheduler->execute();
