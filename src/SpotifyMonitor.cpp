e017386b1b46bb278e2bbf984ed391bf5332b995
Florian Lüdiger <florian.luediger@adesso.de>
2021-04-02 21:51:17 +0200

    Prevent scheduler from checking Spotify while running text is displayed

--- src/SpotifyMonitor.cpp
+++ src/SpotifyMonitor.cpp
@@ -3,6 +3,9 @@
 String lastPlaying = "";
 
 void SpotifyMonitor::checkForNewTrack() {
+    if (RunningText::isRunning() || FlashingText::isFlashing()) {
+        return;
+    }
     String playing = SpotifyClient::getCurrentlyPlaying();
     if (playing != lastPlaying) {
         lastPlaying = playing;
